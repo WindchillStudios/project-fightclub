0d41c06a0b9bcba8adb45314a0e2cb2bb47c4571
JesseLeitch <jesseleitch@cmail.carleton.ca>
2015-04-16 12:14:58 -0400

    Updated server settings and final controller/game scripts

--- Networking/TCPcontroller.cs
+++ Networking/TCPcontroller.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System;
 using System.IO;
 using System.Net;
@@ -11,17 +12,22 @@ public class TCPclient : MonoBehaviour {
 
 	void Awake() {
 		DontDestroyOnLoad(transform.gameObject);
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
 	}
 
+	roomSelect roomScript;
+
+	public GameObject sendToScript;
 	string input, stringData;
+	Queue<string> inputs = new Queue<string>();	
+	Queue<byte[]> outputs = new Queue<byte[]>();
+	bool buffer1full, buffer2full, buffer3full, buffer4full;
+	byte[] writeData, writeBuffer1, writeBuffer2, writeBuffer3, writeBuffer4;
 	TcpClient server;
 	//public Movement playerMove;
 	bool runThread;
-	//bool thereIsData = false;
+	bool thereIsData = false;
 	bool dataToWrite = false;
-
-	bool buffer1full, buffer2full, buffer3full, buffer4full;
-	byte[] writeData, writeBuffer1, writeBuffer2, writeBuffer3, writeBuffer4;
 
 	void Start(){
 		Debug.Log ("Start");
@@ -29,86 +35,80 @@ public class TCPclient : MonoBehaviour {
 		if (server.Connected) {
 			Debug.Log ("Connected");
 			runThread = true;
-			//new Thread (OpenStream).Start();	
+			new Thread (OpenStream).Start();
+			new Thread (DQStream).Start ();
 		}
 	}
 
 	void Update(){
-		/*if(thereIsData == true){
-			Debug.Log ("thereisdata");
-			playerMove.playerInput(stringData);
-			thereIsData = false;
-		}*/
-		if(dataToWrite){
-			NetworkStream ns2 = server.GetStream();
-			Debug.Log ("Data to be wrote");
-			if(ns2.CanWrite){
-				Debug.Log ("CW");
-				if(buffer1full == true){
-					Debug.Log ("Buffer1TBW");
-					ns2.Write(writeBuffer1,0,writeBuffer1.Length);
-					if(buffer2full == true){
-						Debug.Log ("Buffer2TBW");
-						ns2.Write(writeBuffer2,0,writeBuffer2.Length);
-						if(buffer3full == true){
-							Debug.Log ("Buffer3TBW");
-							ns2.Write(writeBuffer3,0,writeBuffer3.Length);
-							if(buffer4full == true){
-								ns2.Write(writeBuffer4,0,writeBuffer4.Length);
-								buffer4full = false;
-							}
-							buffer3full = false;
-						}
-						buffer2full = false;
-					}
-					buffer1full = false;
-				}
-				dataToWrite = false;
-			}else{
-				Debug.Log ("You can not write to the stream.");
+		if(thereIsData == true){
+			//Debug.Log ("thereisdata" + inputs.Dequeue ());
+			string inputString = inputs.Dequeue();
+			Debug.Log (inputString);
+			if(inputString == "Close"){
+				Application.LoadLevel ("Title");
 			}
-		}
+			if(inputString == "Win"){
+				Application.LoadLevel ("Win");
+			}
+			if(inputString == "Loss"){
+				Application.LoadLevel ("Loss");
+			}
+
+			if(inputString == "Progress"){
+				sendToScript = GameObject.Find ("Option 1");
+				roomScript = sendToScript.GetComponent<roomSelect> ();
+
+				Debug.Log ("Progress");
+				roomScript.progressTrue = true;
+			}
+			if(inputString == "Full"){
+				sendToScript = GameObject.Find ("Option 1");
+				roomScript = sendToScript.GetComponent<roomSelect> ();
+
+				Debug.Log ("Full");
+				roomScript.fullTrue = true;
+			}
+			if(inputString == "Open"){
+				Application.LoadLevel ("Character");
+			}
+			//playerMove.playerInput(inputs.Dequeue ());
+			thereIsData = false;
+		}/*
+		if(dataToWrite){
+				NetworkStream ns2 = server.GetStream();
+				Debug.Log ("Data to be wrote");
+				if(ns2.CanWrite){
+					if(outputs.Count > 0){
+					byte[] sentByte = outputs.Dequeue();	
+					//Debug.Log ("CW" + sentByte);
+					ns2.Write(sentByte, 0, sentByte.Length);
+				}else{
+					Debug.Log ("No data in outputs");
+				}
+					if(outputs.Count == 0){
+						dataToWrite = false;
+					}
+				}else{
+					Debug.Log ("You can not write to the stream.");
+				}
+			}*/
 	}
 	
 	public void prepareString(string outputString){
-		Debug.Log ("String prep");
-		//writeData = Encoding.ASCII.GetBytes(outputString);
-		if (buffer1full == true) {
-			Debug.Log ("Buffer1Full");
-			if(buffer2full == true){
-				Debug.Log ("Buffer2Full");
-				if(buffer3full == true){
-					Debug.Log ("Buffer3Full");
-					if(buffer4full == true){
-						Debug.Log ("RIP");
-					}
-					if (buffer4full == false) {
-							writeBuffer4 = Encoding.ASCII.GetBytes(outputString);
-					}
-					buffer4full = true;
-				}
-				if (buffer3full == false) {
-						writeBuffer3 = Encoding.ASCII.GetBytes(outputString);
-				}
-				buffer3full = true;
-			}
-			if (buffer2full == false) {
-					writeBuffer2 = Encoding.ASCII.GetBytes(outputString);
-			}
-			buffer2full = true;
-		}
-		if (buffer1full == false) {
-				writeBuffer1 = Encoding.ASCII.GetBytes (outputString);
-				}
-		buffer1full = true;
+		byte[] encodedString = Encoding.ASCII.GetBytes(outputString);
+		Debug.Log ("Encoded " + encodedString.Length);
+		outputs.Enqueue(encodedString);
+		Debug.Log (outputs.Count);
 		dataToWrite = true;
-	}	/*
+	}
 	private void OpenStream(){
 		Debug.Log ("STREAM!");
 		NetworkStream ns = server.GetStream ();
 		byte[] data = new byte[1024];
 		int recv;
 		while (runThread == true) {
+			Debug.Log("Waiting");
 			recv = 0;
 			try{
 				recv = ns.Read (data, 0, data.Length);
@@ -124,17 +124,40 @@ public class TCPclient : MonoBehaviour {
 				Debug.Log ("Breaking down");
 				break;
 			}
-
-			stringData = Encoding.ASCII.GetString (data, 0, recv);
+			
+			inputs.Enqueue(Encoding.ASCII.GetString (data, 0, recv));
 			Debug.Log(stringData + " stringdata");
 		}
 		Debug.Log("Disconnecting from server...");
 		ns.Close ();
 		server.Close ();
-	}*/
+	}
+
+	private void DQStream(){
+		while (runThread == true) {
+			if(dataToWrite){
+				NetworkStream dq = server.GetStream ();
+				Debug.Log ("Data to be wrote");
+				if (dq.CanWrite) {
+						if (outputs.Count > 0) {
+								Debug.Log ("Peek " + outputs.Peek());
+								byte[] sentByte = outputs.Dequeue ();	
+								dq.Write (sentByte, 0, sentByte.Length);
+						} else {
+								Debug.Log ("No data in outputs");
+						}
+						if (outputs.Count == 0) {
+								dataToWrite = false;
+						}
+				} else {
+						Debug.Log ("You can not write to the stream.");
+			}
+			}
+		}
+	}
 
 	~TCPclient(){
-		runThread = false;
+		server.Close ();
 	}
 
 	void OnApplicationPause() {
